e58f1654364f0b546c6a28668d993224fe2ab3f5
Ekaterina Shmeleva <ekaterina.shmeleva@aalto.fi>
2019-03-10 21:03:36 +0200

    Annotate IdentityController methods

--- src/Services/Identity/Controllers/IdentityController.cs
+++ src/Services/Identity/Controllers/IdentityController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -12,12 +13,24 @@ namespace Identity.Controllers
     [Route("api/v1/[controller]")]
     public class IdentityController : ControllerBase
     {
+        // POST: api/v1/identity/create
+        [HttpPost]
+        [Route("create")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
         public async Task<ActionResult> CreateIdentityAsync()
         {
             throw new NotImplementedException();
         }
 
-        public async Task<ActionResult> GetJwtAsync()
+        // GET: api/v1/identity/jwt?username={string}&password={string}
+        [HttpGet]
+        [Route("jwt")]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult<string>> GetJwtAsync(
+            [FromQuery]string username = null,
+            [FromQuery]string password = null)
         {
             throw new NotImplementedException();
         }
