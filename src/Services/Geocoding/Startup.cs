08fd67422d28b01f740006f8d402008c8a38bd90
Ekaterina Shmeleva <ekaterina.shmeleva@aalto.fi>
2019-04-13 16:05:49 +0300

    Swagger. Swagger Everywhere!

--- src/Services/Geocoding/Startup.cs
+++ src/Services/Geocoding/Startup.cs
@@ -14,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Geocoding
 {
@@ -38,6 +39,11 @@ namespace Geocoding
             });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Geocoding API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,6 +63,12 @@ namespace Geocoding
             {
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Geocoding API v1");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
--- src/Services/Identity/Startup.cs
+++ src/Services/Identity/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Identity
 {
@@ -30,6 +31,11 @@ namespace Identity
             services.AddTransient<IStorageService, MongoDbStorageService>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Identity API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +49,12 @@ namespace Identity
             {
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Identity API v1");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
